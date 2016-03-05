b5c663409ad23cb70d3621e4dc7404f416727567
Jorge Alejandro Chacon <j@achacon.me>
2016-03-05 11:46:40 -0500

    Added stuff to the meals view

--- client/scripts/controllers/mealsCtrl.js
+++ client/scripts/controllers/mealsCtrl.js
@@ -6,6 +6,29 @@ function mealsCtrl($scope, $reactive){
 
     $reactive(this).attach($scope);
 
-    this.title = "damnn Meals";
+    this.titless = "My mealss damn it"
+
+    $scope.meals = [
+        {
+            id: 0,
+            restaurant: "Coyo",
+            picture: "images/coyo-taco.jpg"
+        },
+        {
+            id: 1,
+            restaurant: "Mc Donald's",
+            picture: "images/mc.jpg"
+        },
+        {
+            id: 2,
+            restaurant: "Pollo Tropical",
+            picture: "images/pt.jpg"
+        },
+        {
+            id: 3,
+            restaurant: "Wendy's",
+            picture: "images/ws.jpg"
+        }
+    ];
 }
 
